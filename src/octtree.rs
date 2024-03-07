dd177d5b86b943c803d0757168ebeb6483d1f6b1
Matthew Deville <matthew@stockly.ai>
2024-03-07 21:02:53 +0100

    update toml

--- src/octtree.rs
+++ src/octtree.rs
@@ -1,12 +1,1 @@
-use crate::objects::Object;
 
-struct Node {
-    objects: Vec<Box<dyn Object>>,
-    children: [Option<Box<Node>>; 8],
-}
-
-impl Node {
-    fn add_object(&mut self, object: impl Object) {
-        todo!()
-    }
-}
